77902c9d9470917e60c9a0ab8eb0260b7173020f
Noah Sutton-Smolin <noahsutsmo@gmail.com>
2015-09-21 17:50:01 -0700

    Restructured empty adjudicator; all regions now have empty orders

--- gamestate/adjudicator.go
+++ gamestate/adjudicator.go
@@ -1,27 +1,18 @@
 package gamestate
 
-import "fmt"
+func (o *OrderSet) setOrderDeps() {
 
-func (o *OrderSet) buildDFSTree() {
-	for k := range *o {
-		fmt.Print(k, ", ")
-	}
-
-	// for _, v := range *o {
-	// 	if v.orderSup != nil {
-	// 		oSup := (*o)[*v.orderSup]
-	// 		fmt.Println(oSup)
-	// 		oSup.depOrders = append(oSup.depOrders, v)
-	// 	}
-	// 	if v.orderTo != nil {
-	// 		oTo := (*o)[*v.orderTo]
-	// 		fmt.Println(*v.orderTo, (*o)[*v.orderTo], oTo)
-	// 		oTo.depOrders = append(oTo.depOrders, v)
-	// 	}
-	// }
 }
 
 func (o *OrderSet) stripOrderCycles() {
+
+}
+
+func resolveOrderCycle(o []*Order) {
+
+}
+
+func (o *OrderSet) assignStrengths() {
 
 }
 
@@ -35,10 +26,11 @@ func (o *OrderSet) getResolvedMap(r map[RegionCode]*Region) map[RegionCode]*Regi
 
 // Adjudicate takes the set of orders and returns the new game state
 func (o *OrderSet) Adjudicate() *map[RegionCode]*Region {
-	o.ValidateOrders()
-	o.CleanInvalidOrders()
-	o.buildDFSTree()
+	o.validateOrders()
+	o.cleanupOrders()
+	o.setOrderDeps()
 	o.stripOrderCycles()
+	o.assignStrengths()
 	o.resolveRecursive()
 	RegionIndex = o.getResolvedMap(RegionIndex)
 	return &RegionIndex
